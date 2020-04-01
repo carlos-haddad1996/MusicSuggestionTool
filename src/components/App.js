d2730aa9176f902d3b7bbdc9c0de729e61df54af
Carlos Haddad <carlos.haddad@partnerhero.com>
2020-03-31 21:19:16 -0600

    Applied styling
    - Made Grid List a bit more responsive
    - Added styling to buttons and filters

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import '../App.css';
 import { createBrowserHistory } from 'history';
 import LoginScreen from '../components/containers/loginScreen';
+import '../styles/App.css';
 
 export const history = createBrowserHistory();
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App wrapped-container bg-dark text-white">
                 <LoginScreen />
             </div>
         );
